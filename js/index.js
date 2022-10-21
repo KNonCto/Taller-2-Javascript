ac800fc98bcf1cb4e3cdc72c8bbd1ed49ce73904
Daniel Parra <danielparraalvarez@gmail.com>
2022-10-20 21:34:58 -0300

    Finished

--- js/index.js
+++ js/index.js
@@ -3,11 +3,22 @@ function number(element) {
 }
 
 function operation(element) {
-  document.getElementById("current-function").innerHTML =
+  document.getElementById("current-function").innerHTML +=
     document.getElementById("result").innerHTML + element.innerHTML;
+  document.getElementById("result").innerHTML = "";
 }
 
 function clean() {
   document.getElementById("result").innerHTML = "";
+  document.getElementById("current-function").innerHTML = "";
+}
+
+function equals() {
+  document.getElementById("result").innerHTML = eval(
+    (
+      document.getElementById("current-function").innerHTML +
+      document.getElementById("result").innerHTML
+    ).replace(/x/g, "*")
+  );
   document.getElementById("current-function").innerHTML = "";
 }
